bbba5b7c499ada5fd5abc1d73f9cd5298971d007
Mohan Kiran <164997456+MohanKiran07@users.noreply.github.com>
2024-04-14 23:24:22 +0530

    Update Single Linked List.cxx

--- Single Linked List.cxx
+++ Single Linked List.cxx
@@ -141,8 +141,85 @@ void DP(struct node**h,int p)
  printf("Key is not found\n");
 }
 
-                                                                                    
- 
+
+void REV(struct node**h)
+{
+   struct node*prev=NULL,*nn=NULL,*t=*h;
+   while(t!=NULL)
+   {
+         nn=t->next;
+         t->next=prev;
+         prev=t;
+         t=nn;
+    }
+    *h=prev;
+ }                                                                                                                                                             
+ void RD(struct node**h)
+ {
+ struct node*c=*h,*t,*prev;
+ while(c!=NULL)
+ {
+     t=c->next;
+     prev=c;
+     while(t!=NULL)
+     {
+         if(c->d==t->d)
+         {
+              prev->next=t->next;
+              free(t);
+              t=prev->next;
+         }
+         else
+         {
+             prev=t;
+             t=t->next;
+         }
+      }
+      c=c->next;
+  }                                                                                                                                                     
+  }
+  
+void BS(struct node** head) {
+    if (*head == NULL || (*head)->next == NULL) 
+    {
+        return; 
+    }
+
+    int swapped;
+    struct node *ptr1;
+    struct node *lptr = NULL;
+
+    do
+     {
+        swapped = 0;
+        ptr1 = *head;
+
+        while (ptr1->next != lptr) {
+            if (ptr1->d > ptr1->next->d) 
+            {
+                int temp = ptr1->d;
+                ptr1->d = ptr1->next->d;
+                ptr1->next->d = temp;
+                swapped = 1;
+            }
+            ptr1 = ptr1->next;
+        }
+        lptr = ptr1; 
+    } while (swapped);
+}             
+   
+void LEN(struct node*h)
+{
+    int c=0;
+    struct node*t=h;
+    while(t!=NULL) 
+    {
+         c++;
+         t=t->next;
+     }
+     printf(" Length of Linked List is %d\n",c);
+ }                                                                                                                                                                                                      
+                                                                                                          
  void display(struct node**head)
  {
         struct node*t=*head;
@@ -155,7 +232,32 @@ void DP(struct node**h,int p)
         printf("\n~~~~~~~~~~~~~~~~~~~\n\n");
  }
                  
-        
+void SPLIT(struct node* h) {
+    if (h == NULL || h->next == NULL) {
+        printf("List is too short to split.\n");
+        return;
+    }
+
+    struct node* slowPtr = h;
+    struct node* fastPtr = h->next;
+    while (fastPtr != NULL) {
+        fastPtr = fastPtr->next;
+        if (fastPtr != NULL) {
+            slowPtr = slowPtr->next;
+            fastPtr = fastPtr->next;
+        }
+    }
+
+    struct node* h2 = slowPtr->next;
+    slowPtr->next = NULL;
+
+    printf("First Half of Linked List:\n");
+    display(&h);
+    printf("Second Half of Linked List:\n");
+    display(&h2);
+}
+
+                
 int main()   
 {
            struct node*h=NULL,*nn=NULL,*t=NULL;
@@ -169,7 +271,7 @@ int main()
                scanf("%d",&c);
                }while(c==1);
      
-               printf("\n~~~~~~~~~~~~~~~~~~\nOperations\n1.Insert at biginning\n2.insert at End\n3.Insert at specific position\n4.Delete at biginning\n5.Delete at End\n6.Delete at specific Position\n7.Search Operation\n0.Exit\n~~~~~~~~~~~~~~~~~~\n\n");
+               printf("\n~~~~~~~~~~~~~~~~~~\nOperations\n1.Insert at biginning\n2.insert at End\n3.Insert at specific position\n4.Delete at biginning\n5.Delete at End\n6.Delete at specific Position\n7.Search Operation\n8.Reverse Operation\n9.Remove Duplicates\n10.Sort the Linked List\n11.Length of Linked List\n12.Display\n13.Split\n0.Exit\n~~~~~~~~~~~~~~~~~~\n\n");
                while(1)
                {
                    printf("Enter your Choice:");
@@ -228,6 +330,37 @@ int main()
                        search (h,k);
                        break;
                        
+                       case 8:
+                       REV(&h);
+                       printf("Reverse of Linked List:\n");
+                       display(&h);
+                       break;
+                       
+                       case 9:
+                       RD(&h);
+                       printf("Linked List after Remove of Duplicates:\n");
+                       display(&h);
+                       break;
+                       
+                       case 10:
+                       BS(&h);
+                       printf("Linked List after Sorting\n");
+                       display(&h);
+                       break;
+                       
+                       case 11:
+                       LEN(h);                                             
+                       break;     
+                       
+                       case 12:
+                       display(&h);
+                       break;  
+
+                       case 13:
+                       SPLIT(h);
+                       break;
+                       
+                                                                                 
                        case 0:
                        printf("Program Exit!");
                        exit(1);
